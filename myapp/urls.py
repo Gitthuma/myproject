4c3f333c11f4507e42e92b320fa3b7bca13ca859
Gitthuma <george.githuma@gmail.com>
2021-11-21 20:36:49 +0300

    import view to use it in the url

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,7 @@
 # import path that will allow us to add multiple urls in a list
+# import view to use it in the url
 from django.urls import path
+from . import views
 
 
 # Create a list to hold all the urls
