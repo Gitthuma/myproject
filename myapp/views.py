14387e66f99d9656f0727ee5208752524883b1f1
Gitthuma <george.githuma@gmail.com>
2021-11-21 20:39:54 +0300

    create an index function that takes requests and will also be referenced by the views module in the urls.py file

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# create an index function that takes requests and will also be referenced by the views module in the urls.py file
+
+
+def index(request):
+    pass
